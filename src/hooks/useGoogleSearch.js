60d8cbf959c1ace76fded3ab9d3a7c66ee83a6de
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2022-10-03 23:56:27 -0400

    search page

--- src/hooks/useGoogleSearch.js
+++ src/hooks/useGoogleSearch.js
@@ -1,11 +1,28 @@
+import axios from 'axios'
 import { useEffect, useState } from 'react'
+import API_KEY from '../keys'
 
 const CONTEXT_KEY = 'd42f234176fed4de6'
 
 const useGoogleSearch = (term) => {
-  const [fata, setData] = useState()
+  const [data, setData] = useState()
 
-  useEffect(() => {}, [term])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+        )
+        setData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
+  }, [term])
+
+  return { data }
 }
 
 export default useGoogleSearch
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,73 @@
 import React from 'react'
+import './SearchPage.css'
+
+import { Link } from 'react-router-dom'
+import Search from '../components/Search'
+import useGoogleSearch from '../hooks/useGoogleSearch'
 import { useStateValue } from '../StateProvider'
 
-import './SearchPage.css'
+import SearchIcon from '@mui/icons-material/Search'
+import DescriptionIcon from '@mui/icons-material/Description'
+import ImageIcon from '@mui/icons-material/Image'
+import LocalOfferIcon from '@mui/icons-material/LocalOffer'
+import RoomIcon from '@mui/icons-material/Room'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue()
+  const { data } = useGoogleSearch(term)
 
   return (
     <div className='searchPage'>
       <div className='searchPageHeader'>
-        <h1>{term}</h1>
+        <Link to='/'>
+          <img
+            className='searchPageLogo'
+            src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+            alt=''
+          />
+        </Link>
+        <div className='searchPageHeaderBody'>
+          <Search hideButtons />
+          <div className='searchPageOptions'>
+            <div className='searchPageOptionsLeft'>
+              <div className='searchPageOption'>
+                <SearchIcon />
+                <Link to='/all'>All</Link>
+              </div>
+              <div className='searchPageOption'>
+                <DescriptionIcon />
+                <Link to='/news'>News</Link>
+              </div>
+              <div className='searchPageOption'>
+                <ImageIcon />
+                <Link to='/images'>Images</Link>
+              </div>
+              <div className='searchPageOption'>
+                <LocalOfferIcon />
+                <Link to='/shopping'>shopping</Link>
+              </div>
+              <div className='searchPageOption'>
+                <RoomIcon />
+                <Link to='/maps'>maps</Link>
+              </div>
+              <div className='searchPageOption'>
+                <MoreVertIcon />
+                <Link to='/more'>more</Link>
+              </div>
+            </div>
+            <div className='searchPageOptionsRight'>
+              <div className='searchPageOption'>
+                <Link to='/settings'>Settings</Link>
+              </div>
+              <div className='searchPageOption'>
+                <Link to='/tools'>Tools</Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className='searchPageResults'></div>
     </div>
   )
